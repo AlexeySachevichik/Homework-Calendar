107dcc25450440b2675cc2ea8c7f6efc37952331
Aliaksey Sachavichyk <roim@ya.ru>
2019-01-18 11:47:57 +0300

    show month

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 "use strict"
 
-var listDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+var listDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 var listMonth = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
+
+/**
+ * БЫСТРОЕ ДОБАВЛЕНИЕ ЗАПИСИ
+ */
 var buttonFastAdd = document.getElementById('btn-fast-add');
 var pupopFastAdd = document.getElementById('popup-fast-add');
 var inputFastAdd = document.getElementById('pfa-input');
@@ -12,23 +16,53 @@ var buttonFastAddCancel = document.getElementById('pfa-btn-canсel');
 var buttonFastAddClose = document.getElementById('pfa-btn-close');
 
 
+/**
+ * ОБНОВЛЕНИЕ
+ */
 var buttonUpdate = document.getElementById('btn-update');
 
+
+/**
+ * ПОИСК
+ */
 var inputSerach = document.getElementById('input-search');
 
+
+/**
+ * МЕСЯЦ РАНЕЕ, МЕСЯЦ ПОЗЖЕ, СЕГОДНЯ
+ */
 var buttonEarlier = document.getElementById('btn-earlier');
-var buttonMonth = document.getElementById('btn-month');
-var buttonYear = document.getElementById('btn-year');
 var buttonLater = document.getElementById('btn-later');
 var buttonCurrent = document.getElementById('btn-current');
 
 
+/**
+ * ВЫБОР МЕСЯЦА
+ */
+var buttonMonth = document.getElementById('btn-month');
+var pupopMonth = document.getElementById('popup-month');
 
 
-var
-    dateCurrent = new Date(),      // Текущая дата
-    dateSelected = dateCurrent,    // Выбранная дата
-    timer; // Общий таймер
+/**
+ * ВЫБОР ГОДА
+ */
+var buttonYear = document.getElementById('btn-year');
+
+
+/**
+ * ОБЛАСТЬ КАЛЕНДАРЯ
+ */
+var calendarArea = document.getElementById('calendar');
+
+
+
+
+
+var dateCurrent = new Date();      // Текущая дата
+var dateSelected = new Date();     // Выбранная дата
+dateSelected.setFullYear(dateCurrent.getFullYear(), dateCurrent.getMonth(), dateCurrent.getDate());
+var timer; // Общий таймер
+
 
 
 
@@ -36,19 +70,50 @@ var
 // Устанавливаем текущую дату
 var setCurrentDate = function(){
     dateCurrent = new Date();
-    dateSelected = dateCurrent;
+    dateSelected = new Date();
+    dateSelected.setFullYear(dateCurrent.getFullYear(), dateCurrent.getMonth(), dateCurrent.getDate());
 }
 
 // Устанавливаем дату на месяц раньше
 var setEarlierMonth = function(){
-    dateCurrent = new Date();
     dateSelected.setMonth(dateSelected.getMonth() - 1);
 }
 
 // Устанавливаем дату на месяц позже
 var setLaterMonth = function(){
-    dateCurrent = new Date();
     dateSelected.setMonth(dateSelected.getMonth() + 1);
+}
+
+// Получим даты выбранного месяца
+var getMonthDay = function(){
+    var arrayDays = [];
+    var month = dateSelected.getMonth();
+
+    // Создадим коппию выбранной даты
+    var date = new Date();
+    date.setFullYear(dateSelected.getFullYear(), month, dateSelected.getDate());
+
+    // Установим дату на первое число текущего месяца
+    date.setDate(1);
+
+    // Получим первый день недели
+    var day = date.getDay() == 0 ? 7 : date.getDay();
+    date.setDate(date.getDate() - day + 1);
+
+    // Получим первую неделю
+    for(var i=0; i<7; i++){
+        arrayDays.push([ date.getFullYear(), date.getMonth(), date.getDate() ]);
+        date.setDate(date.getDate() + 1);
+    }
+
+    // Пока нужный месяц добавляем неделю в массив
+    while(date.getMonth() == month){
+        for(var i=0; i<7; i++){
+            arrayDays.push([ date.getFullYear(), date.getMonth(), date.getDate() ]);
+            date.setDate(date.getDate() + 1);
+        }
+    }
+    return arrayDays;
 }
 
 
@@ -56,28 +121,14 @@ var setLaterMonth = function(){
 
 
 
-// Выводим на экран выбранный месяц
+// Выводим на экран метку выбранного месяца
 var changeLableMonth = function(){
     buttonMonth.innerHTML = listMonth[dateSelected.getMonth()];
 }
 
-// Выводим на экран выбранный год
+// Выводим на экран метку выбранного года
 var changeLableYear = function(){
     buttonYear.innerHTML = dateSelected.getFullYear();
-}
-
-// Показываем окно "Быстрого добавления"
-var showPupopFastAdd = function(){
-    if(!pupopFastAdd.classList.contains('active')){
-        pupopFastAdd.classList.add('active');
-    }
-}
-
-// Скрываем окно "Быстрого добавления"
-var hidePupopFastAdd = function(){
-    if(pupopFastAdd.classList.contains('active')){
-        pupopFastAdd.classList.remove('active');
-    }
 }
 
 
@@ -94,6 +145,41 @@ var showStatus = function(element, status, message){
     }, 5000);
 }
 
+// Показываем выбранный месяц
+var showSelectedMonth = function(){
+    var days = getMonthDay();
+    var quantityWeaks = days.length / 7;
+    var count = 0;
+    var month = days[17][1];
+    calendarArea.innerHTML = '';
+
+    for(var j=0; j<quantityWeaks; j++){
+        var row = document.createElement('div');
+        row.classList.add('row');
+
+        for(var i=0; i<7; i++){
+            var col = document.createElement('div');
+            col.classList.add('col');
+            if( days[count][1] != month ) col.classList.add('unfit');
+
+            var colDate = document.createElement('div');
+            colDate.classList.add('col-date');
+            
+            // Дле первой недели добавим названия дней недели
+            if(j==0){
+                colDate.innerHTML = listDay[i] + ', ' + days[count][2];
+            }
+            else {
+                colDate.innerHTML = days[count][2];
+            }
+            col.appendChild(colDate);
+
+            count++;
+            row.appendChild(col);
+        }
+        calendarArea.appendChild(row);
+    }
+}
 
 
 
@@ -101,18 +187,18 @@ var showStatus = function(element, status, message){
 
 // Нажали на кнопу "Быстрого добавления" 
 buttonFastAdd.addEventListener('click', function(e){
-    showPupopFastAdd();
+    pupopFastAdd.classList.toggle('active');
 });
 
 // Нажали на кнопу "Быстрого добавления - Отмена" 
 buttonFastAddCancel.addEventListener('click', function(e){
-    hidePupopFastAdd();
+    pupopFastAdd.classList.toggle('active');
     inputFastAdd.value = "";
 });
 
 // Нажали на кнопу "Быстрого добавления - Закрыть" 
 buttonFastAddClose.addEventListener('click', function(e){
-    hidePupopFastAdd();
+    pupopFastAdd.classList.toggle('active');
 });
 
 // Нажали на кнопу "Быстрого добавления - Добавить" 
@@ -125,12 +211,12 @@ buttonFastAddSubmit.addEventListener('click', function(e){
     }
 });
 
-
 // Нажали на кнопу "Предыдущий месяц" 
 buttonEarlier.addEventListener('click', function(e){
     setEarlierMonth();
     changeLableMonth();
     changeLableYear();
+    showSelectedMonth();
 });
 
 // Нажали на кнопу "Следующий месяц" 
@@ -138,6 +224,7 @@ buttonLater.addEventListener('click', function(e){
     setLaterMonth();
     changeLableMonth();
     changeLableYear();
+    showSelectedMonth();
 });
 
 // Нажали на кнопу "Сегодня" 
@@ -145,98 +232,15 @@ buttonCurrent.addEventListener('click', function(e){
     setCurrentDate();
     changeLableMonth();
     changeLableYear();
+    showSelectedMonth();
 });
 
 
+window.addEventListener('click', function(e){
+    console.log(e.target);
 
-// var dayCurrentLable = listDay[dayCurrent];
-// // console.log('dayCurrentLable', dayCurrentLable);
+    // Нажали на кнопу "Название месяца"
+    if(e.target == buttonMonth) pupopMonth.classList.toggle('active');
+    if(e.target != buttonMonth && pupopMonth.classList.contains('active')) pupopMonth.classList.remove('active');
 
-// var numberCurrentLable = numberCurrent + '';
-// // console.log('numberCurrentLable', numberCurrentLable);
-
-// var monthCurrentLable = listMonth[monthCurrent];
-// // console.log('monthCurrentLable', monthCurrentLable);
-
-// var yearCurrentLable = yearCurrent + '';
-// // console.log('yearCurrentLable', yearCurrentLable);
-// 
-// var View = {
-
-//  // Получим название дня недели (String)
-//  getLableDay: function(indexDay){
-//      return listDay[ indexDay ];
-//  },
-
-//  // Получим название месяца (String)
-//  getLableMonth: function(indexMonth){
-//      return listMonth[ indexMonth ];
-//  },
-
-//  // Получим название года (Number)
-//  getLableYear: function(indexYear){
-//      return indexYear;
-//  },
-
-//  changeLableMonth: function(indexMonth){
-//      this.buttonMonth.innerHTML = getLableMonth(indexMonth);
-//  },
-
-//  changeLableYear: function(indexYear){
-//      this.buttonYear.innerHTML = getLableYear(indexYear);
-//  },
-
-// };
-
-
-// var Model = function(){
-
-//  var dateNow      = new Date();
-//  var dateCurrent  = this.getDateNow();
-//  // var dayCurrent   = this.dateCurrent.getDate();
-//  // var monthCurrent = this.dateCurrent.getMonth();
-//  // var yearCurrent  = this.dateCurrent.getFullYear();
-
-//  this.getDateNow = function(){
-//      return dateNow;
-//  };
-
-//  this.getDateCurrent = function(){
-//      return dateCurrent;
-//  };
-// }
-
-// var model = new Model();
-
-
-
-// var Model = {
-    
-//  dateNow: new Date(),
-//  dateCurrent: 0,
-
-//  setDateCurrent: function(date){
-//      if(typeof(date) != 'object' || this.dateCurrent == 0) {
-//          this.dateCurrent = this.dateNow;
-//      }
-//      else {
-//          this.dateCurrent = date;
-//      }
-//  },
-// };
-
-// var d = new Date(2011, 0, 1, 0, 0, 0, 0);
-// console.log(d);
-
-// console.log(Model.dateNow);
-// console.log(Model.dateCurrent);
-// Model.setDateCurrent(new Date(2011, 0, 1, 0, 0, 0, 0));
-// console.log(Model.dateCurrent);
-
-
-
-
-// var Controller = {
-// };
-// (function(){
-// }());
+});
